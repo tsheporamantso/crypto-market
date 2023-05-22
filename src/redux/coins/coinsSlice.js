fd1517c9a620d3024e125e9b11facba9e72bdb18
tsheporamantso <tshepo.ramantso@outlook.com>
2023-05-22 14:28:54 +0200

    Create coins Slice and export to store

---
+++ src/redux/coins/coinsSlice.js
@@ -0,0 +1,13 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+const initialState = [];
+
+export const coinsSlice = createSlice({
+  name: 'coins',
+  initialState,
+  reducers: {},
+  extraReducers: () => {
+
+  },
+});
+export default coinsSlice.reducer;
