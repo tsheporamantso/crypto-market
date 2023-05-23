23a0eaf1a644d740574747b9d5d609412f15ff31
tsheporamantso <tshepo.ramantso@outlook.com>
2023-05-23 10:03:20 +0200

    Add React and Redux hooks

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,29 +1,46 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
 import { FaSearch } from 'react-icons/fa';
+import { getCoins } from '../redux/coins/coinsSlice';
+import Coins from './Coins';
 import { NavBar } from './Navigation';
 
-export const Home = () => (
-  <div className="mainContainer">
-    <div>
-      <h1 className="mainTitle">Choose Your Coin</h1>
-      <div className="container">
-        <div>
-          <NavBar />
-        </div>
-        <div className="searchBox">
-          <input
-            className="searchInput"
-            type="text"
-            placeholder="search"
-          />
-        </div>
-        <div>
-          <button aria-label="Mute volume" type="button" className="searchButton">
-            <FaSearch />
-          </button>
+export const Home = () => {
+  const coin = useSelector((state) => state.coins);
+  const dispatch = useDispatch();
+  const [search, setSearch] = useState('');
+
+  useEffect(() => {
+    dispatch(getCoins());
+  }, [dispatch]);
+
+  return (
+    <div className="mainContainer">
+      <div>
+        <h1 className="mainTitle">Choose Your Coin</h1>
+        <div className="container">
+          <div>
+            <NavBar />
+          </div>
+          <div className="searchBox">
+            <input
+              className="searchInput"
+              type="text"
+              placeholder="search"
+              onChange={(event) => setSearch(event.target.value)}
+            />
+          </div>
+          <div>
+            <button aria-label="Mute volume" type="button" className="searchButton" onChange={(e) => setSearch(e.target.value)}>
+              <FaSearch />
+            </button>
+          </div>
         </div>
       </div>
+      <div className="coinItems">
+        <Coins coins={coin.filter((coin) => (search.toLowerCase() === '' ? coin : coin.name.toLowerCase().include(search.toLowerCase())))} />
+      </div>
     </div>
-  </div>
-);
+  );
+};
 export default { Home };
