37e29e1420db9d4d13dd2d56511a9af7f74d5d9a
tsheporamantso <tshepo.ramantso@outlook.com>
2023-05-23 11:33:27 +0200

    Import ShowCoin and add it on route

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import { Home } from './components/Home';
+import ShowCoin from './components/ShowCoin';
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Home />} />
+          <Route path="/coin/:id" element={<ShowCoin />} />
         </Routes>
       </BrowserRouter>
     </div>
