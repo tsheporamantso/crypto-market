1a3b8ab75e554d39cb548350348dd45321db9447
tsheporamantso <tshepo.ramantso@outlook.com>
2023-05-23 08:35:06 +0200

    Add Coin directory destructure coin name, price, symbol, image and id props

---
+++ src/components/Coin.js
@@ -0,0 +1,31 @@
+import { Link } from 'react-router-dom';
+import PropTypes from 'prop-types';
+
+const Coin = ({
+  name, price, symbol, image, id,
+}) => {
+  <Link to={`/coin/${id}`} className="link">
+    <li className="coin">
+      <img className="coinImage" src={image} alt={image} />
+      <h2 className="coinName">
+        {name}
+        <br />
+        {symbol}
+      </h2>
+      <p className="coinPrice">
+        price:
+        {price}
+        $
+      </p>
+    </li>
+  </Link>;
+};
+
+Coin.propTypes = {
+  name: PropTypes.string.isRequired,
+  price: PropTypes.string.isRequired,
+  symbol: PropTypes.string.isRequired,
+  image: PropTypes.string.isRequired,
+  id: PropTypes.string.isRequired,
+};
+export default Coin;
